634b32959a960b0b256191e8c66c9dc682856ae8
Jonathan Yu <jonyu7@me.com>
2016-08-29 22:11:35 -0400

    checkpoint-02-blocitoff: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  devise_for :users
+
+  root 'welcome#index'
+
+  resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
